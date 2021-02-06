7ae9d3fda4839372641216c52fff5063500be66f
henrietterebnor <hre027@uib.no>
2021-02-06 11:31:20 +0100

    spillbrettet vises nå om main kjøres.

--- src/main/java/inf112/skeleton/app/HelloWorld.java
+++ src/main/java/inf112/skeleton/app/HelloWorld.java
@@ -2,22 +2,55 @@ package inf112.skeleton.app;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.loaders.TextureLoader;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL30;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.ui.Cell;
 
 public class HelloWorld implements ApplicationListener {
     private SpriteBatch batch;
     private BitmapFont font;
+
+    //Variables below deals with creating the map, and the different layers of the map.
+    private TiledMap map;
+    private TiledMapTileLayer boardLayer, playerLayer, holeLayer, flagLayer; //remember to add more layers here if added to gameboard
+    private OrthogonalTiledMapRenderer renderer;
+    private OrthographicCamera camera;
+    static int mapSizeX = 5;
+    static int mapSizeY= 5;
+    static float cameraHeight = (float) 5; //Can have values 0-5, but has to be set to 5 to work Strange?
+
+    public HelloWorld() {
+    }
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         font = new BitmapFont();
         font.setColor(Color.RED);
-    }
 
+        //Initializing map, and layer variables.
+        map = new TmxMapLoader().load("assets/gameboard.tmx");
+        boardLayer = (TiledMapTileLayer) map.getLayers().get("assets/gameboard.tmx");
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, mapSizeX, mapSizeY);
+        camera.viewportHeight = cameraHeight;
+        camera.update();
+        renderer = new OrthogonalTiledMapRenderer(map, (float)(0.00333));
+        renderer.setView(camera);
+
+    }
 
     @Override
     public void dispose() {
@@ -29,10 +62,7 @@ public class HelloWorld implements ApplicationListener {
     public void render() {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
-
-        batch.begin();
-        font.draw(batch, "Hello World", 200, 200);
-        batch.end();
+        renderer.render();
     }
 
     @Override
