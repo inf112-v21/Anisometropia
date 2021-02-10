d52f79cae56c0c9d9923f454a2fb24ed876f81cc
mowinkle <77986447+mowinkle@users.noreply.github.com>
2021-02-10 13:38:22 +0100

    Added package multiplayer with a chat application (for testing purposes, not original work)

---
+++ src/main/java/multiplayer/Main2.java
@@ -0,0 +1,13 @@
+package multiplayer;
+
+import multiplayer.Gui;
+
+public class Main2 {
+    public static boolean isHost, isConnected = false;
+    public static Gui g;
+    public static void main(String[] args) {
+        g = new Gui();
+        g.create();
+    }
+}
+
