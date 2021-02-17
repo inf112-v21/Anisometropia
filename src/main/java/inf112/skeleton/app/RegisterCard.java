61714a050a92e9ea5609f0f7084b66fdc9146280
Pheliks <felixtiller>
2021-02-17 16:22:28 +0100

    Samlet MoveCard.java og RotateCard.java i en klasse, RegisterCard.java

---
+++ src/main/java/inf112/skeleton/app/RegisterCard.java
@@ -0,0 +1,25 @@
+package inf112.skeleton.app;
+
+/**
+ * Contains methods and variables that pertain to register cards
+ */
+public class RegisterCard {
+    String pictureLocation; // File location of card graphics
+    int amountToMoveOrRotate;
+    boolean movementCard; // Denotes whether card affects movement or direction
+    Player player;
+
+    public RegisterCard(String pictureLocation, int amountToMoveOrRotate, boolean movementCard) {
+        this.pictureLocation = pictureLocation;
+        this.amountToMoveOrRotate = amountToMoveOrRotate;
+        this.movementCard = movementCard;
+    }
+
+    /**
+     * Executes the action of the register card
+     */
+    public void executeRegister() {
+        if(movementCard) { player.moveByDirection(amountToMoveOrRotate); }
+        else { player.rotate(amountToMoveOrRotate); }
+    }
+}
