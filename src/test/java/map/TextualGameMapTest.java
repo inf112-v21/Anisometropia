fdbbe9126f0d200ac8ce9d27ff02e34538e5d983
mowinkle <77986447+mowinkle@users.noreply.github.com>
2021-03-03 10:52:10 +0100

    Added test for TextualGameMap

---
+++ src/test/java/map/TextualGameMapTest.java
@@ -0,0 +1,17 @@
+package map;
+
+import actor.Player;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TextualGameMapTest {
+
+    @Test
+    public void testPlayerOnGameMap() {
+        TextualGameMap textualGameMap = new TextualGameMap(12, 12);
+        Player player = new Player(0, 0, "testPlayer", textualGameMap);
+        textualGameMap.setPlayerPosition(0,0, player);
+        assertEquals(1, textualGameMap.getValue(0,0));
+    }
+}
