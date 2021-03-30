c8efdc23bf9cbc6aa2c98ccd416fe23eaece2d9a
Petter Ruud <petruud@gmail.com>
2021-03-30 17:44:49 +0200

    Added gears that rotates player left and right

---
+++ src/main/java/assets/Gear.java
@@ -0,0 +1,26 @@
+package assets;
+
+import actor.Player;
+import map.GameMap;
+
+public class Gear extends MovingAssets {
+    @Override
+    public void movePlayer(Player player, GameMap gameMap) {
+        if (gameMap.isThereGearOnThisPosition(player.getX(), player.getY())) {
+            int gearID = gameMap.getAssetLayerID(player.getX(), player.getY());
+            gear(gearID, player);
+        }
+    }
+
+    public void gear(int gearID, Player player){
+        switch (gearID){
+            case (gearRotatingLeft):
+                player.rotate(3);
+                break;
+
+            case (gearRotatingRight):
+                player.rotate(1);
+                break;
+        }
+    }
+}
