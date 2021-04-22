ca937f2bac8d7e99303fe2905ad684f89a6bd5cb
Lazar Davidovic <laki@hotmail.rs>
2021-04-23 01:34:07 +0200

    another player queue test added for respawning/dying of players

--- src/test/java/logic/PlayerQueueTest.java
+++ src/test/java/logic/PlayerQueueTest.java
@@ -20,4 +20,24 @@ public class PlayerQueueTest {
         gameLogic.getPlayerQueue().next();
         assertEquals(player1,gameLogic.getCurrentPlayer());
     }
+
+    @Test
+    public void testQueue2() {
+        TextualGameMap simpleMap = new TextualGameMap(12,12);
+        PlayerQueue playerQueue = new PlayerQueue();
+        Player player1 = new Player(1,1,"player1", simpleMap, true, 0);
+        Player player2 = new Player(2,1,"player2", simpleMap, true, 1);
+        playerQueue.add(player1);
+        playerQueue.add(player2);
+        GameLogic logic = new GameLogic(simpleMap, playerQueue);
+        logic.getPlayerQueue().next();
+        logic.getCurrentPlayer().playerDies();
+        logic.endOfTurnCheck();
+        assertEquals(Boolean.TRUE, logic.getCurrentPlayer().isDead);
+        logic.getPlayerQueue().next();
+        assertEquals(Boolean.FALSE, logic.getCurrentPlayer().isDead);
+        logic.getPlayerQueue().next();
+        logic.getCurrentPlayer().respawn();
+        assertEquals(Boolean.FALSE, logic.getCurrentPlayer().isDead);
+    }
 }
